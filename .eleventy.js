a40e6d6b28841f3dad7539bd7330efd9dd23c1bc
Donovan Hutchinson <donovanh@gmail.com>
2021-01-14 21:48:21 +0000

    Trying puppeteer also

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const puppeteer = require('puppeteer');
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -7,18 +9,18 @@ module.exports = function(eleventyConfig) {
   // Copy `img/` to `_site/img`
   eleventyConfig.addPassthroughCopy("img");
 
-  // eleventyConfig.on('afterBuild', async () => {
-  //   console.log('Node: ', process.version);
-  //   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-  //   const page = await browser.newPage();
-  //   await sleep(200);
-  //   await page.goto('http://localhost:8080');
-  //   await page.screenshot({path: './_site/covid-stats-ireland.png'});
-  //   console.log('Screenshot written');
-  //   await browser.close();
-  //   if (process.env['NODE_ENV'] !== 'development') {
-  //     console.log('Closing server')
-  //     process.exit(0);
-  //   }
-  // });
+  eleventyConfig.on('afterBuild', async () => {
+    console.log('Node: ', process.version);
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const page = await browser.newPage();
+    await sleep(200);
+    await page.goto('http://localhost:8080');
+    await page.screenshot({path: './_site/covid-stats-ireland.png'});
+    console.log('Screenshot written');
+    await browser.close();
+    if (process.env['NODE_ENV'] !== 'development') {
+      console.log('Closing server')
+      process.exit(0);
+    }
+  });
 };

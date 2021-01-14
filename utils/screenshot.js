619c09c8682603e441d5f4af1542bed4872225e2
Donovan Hutchinson <donovanh@gmail.com>
2021-01-14 21:59:50 +0000

    Simpler deploy

--- utils/screenshot.js
+++ utils/screenshot.js
@@ -4,7 +4,9 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.goto('https://hop.ie/covid-stats-ireland/');
-  await page.screenshot({path: './img/covid-stats-ireland.png'});
+  await page.screenshot({
+    path: './img/covid-stats-ireland.png'
+  });
   console.log('Screenshot written');
   await browser.close();
 })();

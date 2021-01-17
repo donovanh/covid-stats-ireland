2204880da7dc8a2c6ecc8a3adab7d7052e1a8681
Donovan Hutchinson <donovanh@gmail.com>
2021-01-17 13:21:18 +0000

    Adding shareable summary image

--- .eleventy.js
+++ .eleventy.js
@@ -14,20 +14,19 @@ module.exports = function(eleventyConfig) {
       const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
       const page = await browser.newPage();
       await sleep(200);
-      await page.goto('http://localhost:8080');
-      const path = './_site/covid-stats-ireland.png';
+      await page.goto('http://localhost:8080/share-image/');
       await page.setViewport({
         width: 1200,
         height: 900,
         deviceScaleFactor: 1,
       });
       await page.screenshot({
-        path,
+        path: './_site/covid-stats-ireland.png',
         clip: {
           x: 0,
-          y: 70,
+          y: 0,
           width: 1200,
-          height: 675
+          height: 628
         }
       });
       console.log('Screenshot written');
--- utils/screenshot.js
+++ utils/screenshot.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async function() {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
-  await page.goto('https://hop.ie/covid-stats-ireland/');
+  await page.goto('http://localhost:8080/share-image/');
   await page.setViewport({
     width: 1200,
     height: 900,
@@ -13,9 +13,9 @@ const puppeteer = require('puppeteer');
     path: './_site/covid-stats-ireland.png',
     clip: {
       x: 0,
-      y: 70,
+      y: 0,
       width: 1200,
-      height: 675
+      height: 628
     }
   });
   console.log('Screenshot written');

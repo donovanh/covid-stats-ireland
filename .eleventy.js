12cc025af02e307ee20e3a6072c95b67174dde69
Donovan Hutchinson <donovanh@gmail.com>
2021-01-18 16:29:20 +0000

    Adding CNAME

--- .eleventy.js
+++ .eleventy.js
@@ -5,9 +5,7 @@ function sleep(ms) {
 }
 
 module.exports = function(eleventyConfig) {
-  // Output directory: _site
-  // Copy `img/` to `_site/img`
-  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("CNAME");
 
   eleventyConfig.on('afterBuild', async () => {
     if (process.env['NODE_ENV'] !== 'development') {

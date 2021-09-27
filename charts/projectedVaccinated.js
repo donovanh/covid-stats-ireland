9a8bf53d5186c853e21262d843993a072520dc0e
Donovan Hutchinson <donovanh@gmail.com>
2021-09-27 11:32:06 +0100

    Adjusting adult percentage

--- charts/projectedVaccinated.js
+++ charts/projectedVaccinated.js
@@ -1,11 +1,14 @@
-const D3Node = require('d3-node');
-const { colours } = require('./theme');
+const D3Node = require("d3-node");
+const { colours } = require("./theme");
 
 module.exports = (data) => {
+  console.log("Generating vaccinations projected chart");
 
-  console.log('Generating vaccinations projected chart');
-
-  const options = { selector: '#projected-vaccinations', container: '<div id="container"><div id="projected-vaccinations"></div></div>' };
+  const options = {
+    selector: "#projected-vaccinations",
+    container:
+      '<div id="container"><div id="projected-vaccinations"></div></div>',
+  };
   const d3n = new D3Node(options); // initializes D3 with container element
   const d3 = d3n.d3;
 
@@ -15,9 +18,9 @@ module.exports = (data) => {
   // Establish target number of fully vaccinated
   // Establish vector
   // Is it (average daily doses / 2) per day?
-  //const pop = data.irelandPop || 4970499;
-  const pop = 3855500; // ADULTS estimated
-  const estimatedGoalPop = Math.floor(pop * 0.95);// 95% of population
+  // const pop = data.irelandPop || 4970499;
+  const pop = 3955500; // ADULTS estimated
+  const estimatedGoalPop = Math.floor(pop * 0.95); // 95% of population
 
   // Current rate per day, calculate based on 7 day average
   let sevenDayTotalDoses = 0;
@@ -37,246 +40,256 @@ module.exports = (data) => {
 
   const w = 400;
   const h = 300;
-  const margin = ({ top: 10, right: 20, bottom: 30, left: 40 });
+  const margin = { top: 10, right: 20, bottom: 30, left: 40 };
 
   // Set up scales
-  const xScale = d3.scaleTime()
-    .domain([
-      d3.min(dataset, d => new Date(d.date)),
-      estimated95Date
-    ])
+  const xScale = d3
+    .scaleTime()
+    .domain([d3.min(dataset, (d) => new Date(d.date)), estimated95Date])
     .range([margin.left, w - margin.right]);
 
   // Scale Y to the weekly average line
   const yMax = Math.ceil(estimatedGoalPop / 1000000) * 1000000;
 
-  const yScale = d3.scaleLinear()
+  const yScale = d3
+    .scaleLinear()
     .domain([0, yMax])
     .range([h - margin.bottom, margin.top]);
 
   // Draw containing svg
-  const svg = d3.select(d3n.document.querySelector('#projected-vaccinations'))
-    .append('svg')
-    .attr('viewBox', `0 0 ${w} ${h}`);
+  const svg = d3
+    .select(d3n.document.querySelector("#projected-vaccinations"))
+    .append("svg")
+    .attr("viewBox", `0 0 ${w} ${h}`);
 
   // Draw axes
-  const xAxis = d3.axisBottom(xScale)
-    .tickPadding(5)
-    .tickSize(5)
-    .ticks(4);
+  const xAxis = d3.axisBottom(xScale).tickPadding(5).tickSize(5).ticks(4);
 
-  const yAxis = d3.axisLeft(yScale)
+  const yAxis = d3
+    .axisLeft(yScale)
     .tickValues([0, 2500000, 5000000])
     .tickPadding(5)
     .tickSize(0 - (w - margin.left - margin.right))
     .tickFormat(d3.format(".2s"));
 
-  const yAxis2 = d3.axisLeft(yScale)
-    .tickValues([d3.max(dataset, d => d.fullyVaccinated)])
+  const yAxis2 = d3
+    .axisLeft(yScale)
+    .tickValues([d3.max(dataset, (d) => d.fullyVaccinated)])
     .tickPadding(5)
     .tickSize(5 - (xScale(new Date()) - margin.left))
     .tickFormat(d3.format(".2s"));
 
-  svg.append('clipPath')
-    .attr('id', 'chart-area')
-    .append('rect')
-    .attr('x', margin.left)
-    .attr('y', margin.top)
-    .attr('width', w - margin.left - margin.right)
-    .attr('height', h - margin.top - margin.bottom);
+  svg
+    .append("clipPath")
+    .attr("id", "chart-area")
+    .append("rect")
+    .attr("x", margin.left)
+    .attr("y", margin.top)
+    .attr("width", w - margin.left - margin.right)
+    .attr("height", h - margin.top - margin.bottom);
 
   // x axis
-  svg.append('g')
-    .classed('x-axis', true)
-    .attr('transform', `translate(0, ${h - margin.bottom})`)
+  svg
+    .append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0, ${h - margin.bottom})`)
     .call(xAxis);
 
-  svg.select('.x-axis')
-    .select('.domain')
-    .remove();
+  svg.select(".x-axis").select(".domain").remove();
 
-  svg.selectAll('.x-axis .tick line')
-    .attr('stroke', colours.darkGrey)
+  svg.selectAll(".x-axis .tick line").attr("stroke", colours.darkGrey);
 
-  svg.selectAll('.x-axis .tick text')
-    .attr('fill', colours.darkGrey)
+  svg.selectAll(".x-axis .tick text").attr("fill", colours.darkGrey);
 
   // y axis
-  svg.append('g')
-    .classed('y-axis', true)
-    .attr('transform', `translate(${margin.left}, 0)`)
+  svg
+    .append("g")
+    .classed("y-axis", true)
+    .attr("transform", `translate(${margin.left}, 0)`)
     .call(yAxis);
 
-  svg.select('.y-axis')
-    .select('.domain')
-    .remove();
+  svg.select(".y-axis").select(".domain").remove();
 
-  svg.select('.y-axis')
-    .select('.tick:first-of-type')
-    .remove();
+  svg.select(".y-axis").select(".tick:first-of-type").remove();
 
-  svg.selectAll('.y-axis .tick line')
-    .attr('stroke', colours.lightGrey);
+  svg.selectAll(".y-axis .tick line").attr("stroke", colours.lightGrey);
 
-  svg.selectAll('.y-axis .tick text')
-    .attr('fill', colours.darkGrey);
+  svg.selectAll(".y-axis .tick text").attr("fill", colours.darkGrey);
 
   // y axis 2
-  svg.append('g')
-    .classed('y-axis-2', true)
-    .attr('transform', `translate(${margin.left}, 0)`)
+  svg
+    .append("g")
+    .classed("y-axis-2", true)
+    .attr("transform", `translate(${margin.left}, 0)`)
     .call(yAxis2);
 
-  svg.select('.y-axis-2')
-    .select('.domain')
-    .remove();
+  svg.select(".y-axis-2").select(".domain").remove();
 
-  svg.selectAll('.y-axis-2 .tick line')
-    .attr('stroke-width', 0.5)
-    .attr('stroke-dasharray', 1)
-    .attr('stroke', colours.darkGrey);
+  svg
+    .selectAll(".y-axis-2 .tick line")
+    .attr("stroke-width", 0.5)
+    .attr("stroke-dasharray", 1)
+    .attr("stroke", colours.darkGrey);
 
-  svg.selectAll('.y-axis-2 .tick text')
-    .attr('fill', colours.darkGrey);
+  svg.selectAll(".y-axis-2 .tick text").attr("fill", colours.darkGrey);
 
   // Total doses area
-  const totalDosesArea = d3.area()
-    .x(d => (xScale(new Date(d.date))))
+  const totalDosesArea = d3
+    .area()
+    .x((d) => xScale(new Date(d.date)))
     .y0(() => yScale.range()[0])
-    .y1(d => yScale(d.estimatedFullyVaccinated));
+    .y1((d) => yScale(d.estimatedFullyVaccinated));
 
-  svg.append('path')
+  svg
+    .append("path")
     .datum(dataset)
-    .attr('class', 'total-doses')
-    .attr('fill', colours.green)
-    .attr('d', totalDosesArea);
+    .attr("class", "total-doses")
+    .attr("fill", colours.green)
+    .attr("d", totalDosesArea);
 
   // Projected line - extend from current day up to projected date
   const projectedDosesLineData = [
     {
-      x: d3.max(dataset, d => new Date(d.date)),
-      y: d3.max(dataset, d => d.estimatedFullyVaccinated)
+      x: d3.max(dataset, (d) => new Date(d.date)),
+      y: d3.max(dataset, (d) => d.estimatedFullyVaccinated),
     },
     {
       x: estimated95Date,
-      y: estimatedGoalPop
-    }
+      y: estimatedGoalPop,
+    },
   ];
 
-  const projectedDosesLine = d3.line()
-    .x(d => xScale(d.x))
-    .y(d => yScale(d.y));
+  const projectedDosesLine = d3
+    .line()
+    .x((d) => xScale(d.x))
+    .y((d) => yScale(d.y));
 
-  svg.append('path')
+  svg
+    .append("path")
     .datum(projectedDosesLineData)
-    .attr('class', 'projected-vaccinations-line')
-    .attr('fill', 'none')
-    .attr('stroke-width', 0.5)
-    .attr('stroke-dasharray', 6)
-    .attr('stroke', colours.darkGrey)
-    .attr('d', projectedDosesLine);
+    .attr("class", "projected-vaccinations-line")
+    .attr("fill", "none")
+    .attr("stroke-width", 0.5)
+    .attr("stroke-dasharray", 6)
+    .attr("stroke", colours.darkGrey)
+    .attr("d", projectedDosesLine);
 
   // Highlight the end of the estimated line
-  svg.append('circle')
-    .style('fill', colours.darkGrey)
-    .attr('r', 2)
-    .attr('cx', xScale(estimated95Date))
-    .attr('cy', yScale(estimatedGoalPop));
+  svg
+    .append("circle")
+    .style("fill", colours.darkGrey)
+    .attr("r", 2)
+    .attr("cx", xScale(estimated95Date))
+    .attr("cy", yScale(estimatedGoalPop));
 
   // Add info text about this estimate
-  const formatNumber = (number) => Intl.NumberFormat('en-UK').format(number);
-  const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-  const formatDate = (date) => new Date(date).toLocaleDateString("en-US", dateOptions);
+  const formatNumber = (number) => Intl.NumberFormat("en-UK").format(number);
+  const dateOptions = { year: "numeric", month: "long", day: "numeric" };
+  const formatDate = (date) =>
+    new Date(date).toLocaleDateString("en-US", dateOptions);
 
-  const text = svg.append('text')
-      .attr('x', xScale(estimated95Date) - 150)
-      .attr('y', yScale(estimatedGoalPop) + 3)
-      .style('font-size', 10)
-      .style('fill', colours.darkGrey);
+  const text = svg
+    .append("text")
+    .attr("x", xScale(estimated95Date) - 150)
+    .attr("y", yScale(estimatedGoalPop) + 3)
+    .style("font-size", 10)
+    .style("fill", colours.darkGrey);
 
-  svg.append('text')
-    .text(formatDate(estimated95Date) + '*')
-    .attr('x', xScale(estimated95Date) - 10)
-    .attr('y', yScale(estimatedGoalPop) + 3)
-    .attr('dy', 0)
-    .style('font-size', 10)
-    .attr('text-anchor', 'end')
-    .style('fill', colours.darkGrey);
+  svg
+    .append("text")
+    .text(formatDate(estimated95Date) + "*")
+    .attr("x", xScale(estimated95Date) - 10)
+    .attr("y", yScale(estimatedGoalPop) + 3)
+    .attr("dy", 0)
+    .style("font-size", 10)
+    .attr("text-anchor", "end")
+    .style("fill", colours.darkGrey);
 
-  svg.append('text')
-    .text('* based on 7-day average rate of ' + formatNumber(dosesPerDay) + ' doses per day')
-    .attr('x', w - margin.right)
-    .attr('y', h - margin.bottom - 10)
-    .attr('dy', 0)
-    .style('font-size', 10)
-    .attr('text-anchor', 'end')
-    .style('fill', colours.darkGrey);
+  svg
+    .append("text")
+    .text(
+      "* based on 7-day average rate of " +
+        formatNumber(dosesPerDay) +
+        " doses per day"
+    )
+    .attr("x", w - margin.right)
+    .attr("y", h - margin.bottom - 10)
+    .attr("dy", 0)
+    .style("font-size", 10)
+    .attr("text-anchor", "end")
+    .style("fill", colours.darkGrey);
 
-  // Legend: Total fully vaccinated 
-  svg.append('rect')
-    .attr('x', margin.left + 20)
-    .attr('y', margin.top + 8)
-    .style('fill', colours.green50)
-    .attr('width', 10)
-    .attr('height', 10);
+  // Legend: Total fully vaccinated
+  svg
+    .append("rect")
+    .attr("x", margin.left + 20)
+    .attr("y", margin.top + 8)
+    .style("fill", colours.green50)
+    .attr("width", 10)
+    .attr("height", 10);
 
-  svg.append('text')
-    .attr('x', margin.left + 35)
-    .attr('y', margin.top + 14)
-    .attr('alignment-baseline', 'middle')
-    .style('fill', colours.darkGrey)
-    .style('font-size', 10)
-    .text('Total adults vaccinated (' + percentVaccinated.toFixed(2) + '%)');
+  svg
+    .append("text")
+    .attr("x", margin.left + 35)
+    .attr("y", margin.top + 14)
+    .attr("alignment-baseline", "middle")
+    .style("fill", colours.darkGrey)
+    .style("font-size", 10)
+    .text("Total adults vaccinated (" + percentVaccinated.toFixed(2) + "%)");
 
   // Legend: Projected rate
   const legendAvgLinePoints = [
     {
       x: margin.left + 20,
-      y: margin.top + 38
+      y: margin.top + 38,
     },
     {
       x: margin.left + 30,
-      y: margin.top + 28
-    }
+      y: margin.top + 28,
+    },
   ];
-  const legendAvgLine = d3.line()
-    .x(d => d.x)
-    .y(d => d.y);
+  const legendAvgLine = d3
+    .line()
+    .x((d) => d.x)
+    .y((d) => d.y);
 
-  svg.append('path')
+  svg
+    .append("path")
     .datum(legendAvgLinePoints)
-    .attr('class', 'daily-vaccinations-line')
-    .attr('fill', 'none')
-    .attr('stroke-width', 0.5)
-    .attr('stroke-dasharray', 2)
-    .attr('stroke', colours.darkGrey)
-    .attr('d', legendAvgLine);
+    .attr("class", "daily-vaccinations-line")
+    .attr("fill", "none")
+    .attr("stroke-width", 0.5)
+    .attr("stroke-dasharray", 2)
+    .attr("stroke", colours.darkGrey)
+    .attr("d", legendAvgLine);
 
-  svg.append('text')
-    .attr('x', margin.left + 35)
-    .attr('y', margin.top + 34)
-    .attr('alignment-baseline', 'middle')
-    .style('fill', colours.darkGrey)
-    .style('font-size', 10)
-    .text('Projected rate');
+  svg
+    .append("text")
+    .attr("x", margin.left + 35)
+    .attr("y", margin.top + 34)
+    .attr("alignment-baseline", "middle")
+    .style("fill", colours.darkGrey)
+    .style("font-size", 10)
+    .text("Projected rate");
 
   // Legend: 'Target: 95% (' + formatNumber(estimatedGoalPop) + ' people)'
-  svg.append('circle')
-    .style('fill', colours.darkGrey)
-    .attr('r', 2)
-    .attr('cx', margin.left + 24)
-    .attr('cy', margin.top + 53);
+  svg
+    .append("circle")
+    .style("fill", colours.darkGrey)
+    .attr("r", 2)
+    .attr("cx", margin.left + 24)
+    .attr("cy", margin.top + 53);
 
-  svg.append('text')
-    .attr('x', margin.left + 35)
-    .attr('y', margin.top + 54)
-    .attr('alignment-baseline', 'middle')
-    .style('fill', colours.darkGrey)
-    .style('font-size', 10)
-    .text('Target: 95% (' + formatNumber(estimatedGoalPop) + ' adults)');
+  svg
+    .append("text")
+    .attr("x", margin.left + 35)
+    .attr("y", margin.top + 54)
+    .attr("alignment-baseline", "middle")
+    .style("fill", colours.darkGrey)
+    .style("font-size", 10)
+    .text("Target: 95% (" + formatNumber(estimatedGoalPop) + " adults)");
 
-
-  d3n.html()
+  d3n.html();
   const html = `
     <h2>Herd immunity</h2>
     <style>
@@ -373,5 +386,4 @@ module.exports = (data) => {
   `;
 
   return html;
-
-}
+};

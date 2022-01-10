2c08ed179e293aa406d80d833d0ea13edb5afb37
Donovan Hutchinson <donovanh@gmail.com>
2022-01-10 07:03:02 +0000

    Fixing screenshot step

--- utils/screenshot.js
+++ utils/screenshot.js
@@ -1,23 +1,42 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-(async function() {
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function doScreenshot() {
+  await sleep(1000);
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
-  await page.goto('http://localhost:8080/share-image/');
+
+  try {
+    await page.goto("http://localhost:8080/share-image/");
+  } catch (err) {
+    doScreenshot();
+    return;
+  }
   await page.setViewport({
     width: 1200,
     height: 900,
     deviceScaleFactor: 1,
   });
   await page.screenshot({
-    path: './_site/covid-stats-ireland.png',
+    path: "./_site/covid-stats-ireland.png",
     clip: {
       x: 0,
       y: 0,
       width: 1200,
-      height: 628
-    }
+      height: 628,
+    },
   });
-  console.log('Screenshot written');
+  console.log("Screenshot written");
   await browser.close();
+  console.log("Closing server");
+  process.exit(0);
+}
+
+(async function () {
+  await doScreenshot();
 })();

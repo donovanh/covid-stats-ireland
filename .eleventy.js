811d7dd5956de4da1e8bcfa2a9377ef68434b3e8
Donovan Hutchinson <donovanh@gmail.com>
2022-01-10 07:17:28 +0000

    Updating to use fixed version of eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
 const puppeteer = require("puppeteer");
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.addPassthroughCopy("img");
@@ -22,7 +26,32 @@ module.exports = function (eleventyConfig) {
     strict_filters: true,
   });
 
-  // eleventyConfig.on("eleventy.after", async () => {
-
-  // });
+  eleventyConfig.on("afterBuild", async () => {
+    if (process.env["NODE_ENV"] !== "development") {
+      const browser = await puppeteer.launch({
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      });
+      const page = await browser.newPage();
+      await sleep(500);
+      await page.goto("http://localhost:8080/share-image/");
+      await page.setViewport({
+        width: 1200,
+        height: 900,
+        deviceScaleFactor: 1,
+      });
+      await page.screenshot({
+        path: "./_site/covid-stats-ireland.png",
+        clip: {
+          x: 0,
+          y: 0,
+          width: 1200,
+          height: 628,
+        },
+      });
+      console.log("Screenshot written");
+      await browser.close();
+      console.log("Closing server");
+      process.exit(0);
+    }
+  });
 };
